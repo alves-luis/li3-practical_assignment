a329c6939576120f73c3bad3af8b9de0e827059a
Luís Alves <luismig.alves@gmail.com>
2018-06-27 22:15:57 +0100

    Deleted files from Diggy digz

--- Grupo49-master/include/common.h
+++
@@ -1,4 +0,0 @@
-#ifndef __MY_COMMON__
-#define __MY_COMMON__
-char * mystrdup (const char *s);
-#endif
--- Grupo49-master/proj-java-diogo/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf *.txt target/
-
-mvn clean package
-
-mycommand="java -jar target/project2-1.0-SNAPSHOT-jar-with-dependencies.jar "$@
-echo $mycommand
-$mycommand
